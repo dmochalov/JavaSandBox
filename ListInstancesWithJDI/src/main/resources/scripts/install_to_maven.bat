fbb7cb5b80475d3b09c1e9929cc4242377465c1a
dmochalov <rigthcoder@gmail.com>
2020-01-05 14:52:37 +0300

    Initial commit added sources of example with JDI

---
+++ ListInstancesWithJDI/src/main/resources/scripts/install_to_maven.bat
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=D:\YandexDisk\StackOverflow\java\SimpleConnectToJDPA\ConnectToJDPA\libs\tools.jar -DgroupId=jdktools -DartifactId=tools -Dversion=1.0 -Dpackaging=jar
+
+pause
